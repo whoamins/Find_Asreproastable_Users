c7af3e0ee557d534b2e7747a88aae29745e0624a
whoamins <40am1ns@protonmail.com>
2022-01-08 23:06:27 +0300

    Initial commit

---
+++ Find_Asreproastable_Users/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Find_Asreproastable_Users;
+
+var searcher = new Searcher();
+try
+{
+    searcher.GetVulnerableUsers(args[0], args[1], args[2]);
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("Example of usage: Find_Asreproastable_Users.exe domain username password");
+}
---
+++ Find_Asreproastable_Users/Searcher.cs
@@ -0,0 +1,72 @@
+﻿using LdapForNet;
+
+
+namespace Find_Asreproastable_Users;
+
+public class Searcher
+{
+    private LdapConnection? Connect(string domain, string username, string password)
+    {
+        var cn = new LdapConnection();
+
+        if (domain.Split(".").Length == 1)
+        {
+            Console.WriteLine("The domain must have TLD\nTrying to add '.com'\n");
+            domain += ".com";
+        }
+
+        try
+        {
+            cn.Connect(new Uri($"LDAP://{domain}"));
+            cn.Bind(userDn: username, password: password);
+        }
+        catch (LdapInvalidCredentialsException)
+        {
+            Console.WriteLine("Invalid Credentials!");
+            return null;
+        }
+        catch (LdapException)
+        {
+            Console.WriteLine("Can't connect to this domain!");
+            return null;
+        }
+
+        return cn;
+    }
+    
+    /// <summary>
+    /// Returns users vulnerable to asreproasting attack
+    /// </summary>
+    /// <param name="domain">Domain Controller Domain Name. Example: amazing.dc</param>
+    /// <param name="username">Username for LDAP connection</param>
+    /// <param name="password">Password for LDAP connection</param>
+    public void GetVulnerableUsers(string domain, string username, string password)
+    {
+        using var cn = Connect(domain, username, password);
+        
+        if (cn == null) return;
+
+        var splittedDomain = domain.Split('.');
+        IList<LdapEntry> entries = new List<LdapEntry>();
+
+        switch (splittedDomain.Length)
+        {
+            case 3:
+                entries = cn.Search($"dc={splittedDomain[0]},dc={splittedDomain[1]},dc={splittedDomain[2]}",
+                    "(&(UserAccountControl:1.2.840.113556.1.4.803:=4194304))");
+                break;
+            case 2:
+                entries = cn.Search($"dc={splittedDomain[0]},dc={splittedDomain[1]}",
+                    "(&(UserAccountControl:1.2.840.113556.1.4.803:=4194304))");
+                break;
+        }
+        
+        if (entries.Count == 0) return;
+        
+        Console.WriteLine("Vulnerable Users:");
+        foreach (var e in entries)
+        {
+            Console.WriteLine(e.DirectoryAttributes["CN"].GetValue<string>());
+        }
+    }
+}
